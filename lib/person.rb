bab713fa197c59187a266139acf2491b56193c79
Miriam Aceves <miriammerari02@gmail.com>
2021-06-03 11:53:15 -0400

    Create add_specialties method

--- lib/person.rb
+++ lib/person.rb
@@ -1,8 +1,15 @@
 class Person
-  attr_reader :name, :experience
+  attr_reader :name,
+              :experience,
+              :specialties
+
   def initialize(name, experience)
     @name = name
     @experience = experience
     @specialties = []
+  end
+
+  def add_specialty(new_specialty)
+    @specialties << new_specialty
   end
 end
