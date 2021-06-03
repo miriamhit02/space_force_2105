f8d85c94fdec4d08fc9bae35faa18c2e6f313ccc
Miriam Aceves <miriammerari02@gmail.com>
2021-06-03 11:32:17 -0400

    Add attributes test

--- spec/spacecraft_spec.rb
+++ spec/spacecraft_spec.rb
@@ -6,4 +6,11 @@ RSpec.describe Spacecraft do
 
     expect(daedalus).to be_a(Spacecraft)
   end
+
+  it 'has attributes'  do
+    daedalus = Spacecraft.new({name: 'Daedalus', fuel: 400})
+
+    expect(daedalus.name).to eq('Daedalus')
+    expect(daedalus.fuel).to eq(400)
+  end
 end
